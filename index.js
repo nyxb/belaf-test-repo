f8fcacafb034c5d7ae51f3e0da11a44e1eb35749
nyxb <dennis@ollhoff.life>
2025-12-19 12:50:02 +0100

    chore: update index.js to export all modules

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // belaf-test-repo
 // Test project for belaf release management
 
+const { greet, farewell } = require('./src/greet')
+const { capitalize, slugify } = require('./src/utils')
+
 module.exports = {
   name: 'belaf-test-repo',
-  version: require('./package.json').version
+  version: require('./package.json').version,
+  greet,
+  farewell,
+  capitalize,
+  slugify
 }
