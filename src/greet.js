bcabbeb65d7355c94027f62a72d15360b9d9b01d
nyxb <dennis@ollhoff.life>
2025-12-19 12:49:48 +0100

    fix: add input validation and default values to greeting functions

--- src/greet.js
+++ src/greet.js
@@ -2,11 +2,17 @@
  * Greeting module for belaf-test-repo
  */
 
-function greet(name) {
+function greet(name = 'World') {
+  if (!name || typeof name !== 'string') {
+    throw new Error('Name must be a non-empty string')
+  }
   return `Hello, ${name}! Welcome to belaf-test-repo.`
 }
 
-function farewell(name) {
+function farewell(name = 'Friend') {
+  if (!name || typeof name !== 'string') {
+    throw new Error('Name must be a non-empty string')
+  }
   return `Goodbye, ${name}! See you soon.`
 }
 
