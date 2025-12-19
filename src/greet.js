dcc3c4634ebb8c7582cee6dfb6433df659c63d4b
nyxb <dennis@ollhoff.life>
2025-12-19 12:49:32 +0100

    feat: add greeting module with greet and farewell functions

---
+++ src/greet.js
@@ -0,0 +1,13 @@
+/**
+ * Greeting module for belaf-test-repo
+ */
+
+function greet(name) {
+  return `Hello, ${name}! Welcome to belaf-test-repo.`
+}
+
+function farewell(name) {
+  return `Goodbye, ${name}! See you soon.`
+}
+
+module.exports = { greet, farewell }
